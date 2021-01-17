dd283eb5a89df4d59ec120bc23d509b8d395ef6e
Emily Rindner <Emily@MacBook-Pro-9.local>
2021-01-16 17:19:27 -0800

    13.4.3

--- Simple_map/static/js/logic.js
+++ Simple_map/static/js/logic.js
@@ -2,7 +2,20 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+// Coordinates for each point to be used in the polyline.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+ ];
+
+// Create a polyline using the line coordinates and make the line black.
+L.polyline(line, {
+    color: "yellow"
+ }).addTo(map);
 
 // An array containing each city's location, state, and population.
 let cities = [{
@@ -68,3 +81,4 @@ cityData.forEach(function(city) {
     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
   .addTo(map);
 });
+
