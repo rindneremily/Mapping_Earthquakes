7d821755f21f5dde48c1b16cfc1c4296d9e1ca31
Emily Rindner <Emily@MacBook-Pro-9.local>
2021-01-16 17:38:40 -0800

    13.5.3

--- Simple_map/static/js/logic.js
+++ Simple_map/static/js/logic.js
@@ -1,21 +1,29 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
 // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
-// Coordinates for each point to be used in the polyline.
-let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
- ];
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
-// Create a polyline using the line coordinates and make the line black.
-L.polyline(line, {
-    color: "yellow"
- }).addTo(map);
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
 
 // An array containing each city's location, state, and population.
 let cities = [{
@@ -55,6 +63,13 @@ cities.forEach(function(city) {
     console.log(city)
    });
 
+// Grabbing our GeoJSON data.
+L.geoJson(sanFranAirport, {
+    onEachFeature: function(feature, layer) {
+      console.log(layer);
+      layer.bindPopup();
+    }
+  }).addTo(map);
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
